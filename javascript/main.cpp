c14dcf1691b37831e49f755dc40093ba46296b8f
Alexsey <84779107+razenxc@users.noreply.github.com>
2023-02-16 12:18:05 +0200

    Update main.cpp

--- javascript/main.cpp
+++ javascript/main.cpp
@@ -1,6 +1,22 @@
+// Don't ask why cpp in web site
 #include <iostream>
+#include <string>
 
-main () {
-	std::cout << 'CPP Started' << std::endl;
-	return 0
+int main() {
+	std::string websiteName;
+	std::cout << "Please enter the name of your website: ";
+	std::cin >> websiteName;
+
+	std::cout << std::endl << "Creating website code for " << websiteName << std::endl;
+
+	std::cout << "<html>" << std::endl;
+	std::cout << "<head>" << std::endl;
+	std::cout << "<title>" << websiteName << "</title>" << std::endl;
+	std::cout << "</head>" << std::endl;
+	std::cout << "<body>" << std::endl;
+	std::cout << "<h1>Welcome to " << websiteName << "!</h1>" << std::endl;
+	std::cout << "</body>" << std::endl;
+	std::cout << "</html>" << std::endl;
+
+return 0;
 }
